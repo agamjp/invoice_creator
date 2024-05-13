7bfb842a3485d3c252f59a3659f35d023abfceec
Agnieszka <agamjp@gmail.com>
2024-05-13 18:44:57 +0200

    create minimal PDFs with invoice number and date for each excel file

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-import os
 from fpdf import FPDF
 import pandas as pd
+from pathlib import Path
+import glob
+# alternative: import os
+# files = os.listdir("data")
+# (adjust the loop accordingly)
 
-files = os.listdir("data")
+files = glob.glob("data/*.xlsx")
 
 for f in files:
-    if f.endswith(".xlsx"):
-        df = pd.read_excel(f"data/{f}")
-        print(df)
+    inv_filename = Path(f).stem
+    number_date = inv_filename.split("-")
+    invoice_nr = number_date[0]
+    invoice_date = number_date[1]
+    txt = f"Invoice No. {invoice_nr}\nDate: {invoice_date}"
+    df = pd.read_excel(f, sheet_name="Sheet 1")
+    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="a4")
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=14)
+    pdf.multi_cell(w=0, h=14, txt=txt, align="L")
+
+    pdf.output(f"output/{inv_filename}.pdf")
